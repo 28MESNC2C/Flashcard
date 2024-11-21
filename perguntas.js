6d5c0e413137633d1d8129b9cc55e8edbecd6e6e
28MESNC2C <00001118761960sp@al.educacao.sp.gov.br>
2024-11-21 16:11:37 -0300

    FlashCard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da China?',
+    'A capital da China é Pequim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma TypeScript?',
+    'TypeScript é uma linguagem de programação de código aberto da Microsoft que usa tipos para descrever objetos e validar código.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é bye'
+)
